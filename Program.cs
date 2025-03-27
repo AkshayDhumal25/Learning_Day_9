f51ea15550e019e95bd6e209a07c95fe8ea177fe
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-03-27 09:32:36 +0530

    Encapsulation

--- Program.cs
+++ Program.cs
@@ -1,1 +1,54 @@
-﻿
+﻿// Encapsulation in c#
+using System;
+namespace Encapsulation
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+
+        public void SetName(string pName)
+        {
+            this.name = pName;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetAge(int pAge)
+        {
+            if(pAge > 0 && pAge < 150)
+            {
+                this.age = pAge;
+            }
+            else
+            {
+                throw new Exception("Invalid Age");
+            }
+            
+        }
+        public int GetAge()
+        {
+            return this.age;
+        }
+        public void DisplayPersonDetails()
+        {
+            Console.WriteLine($"name = {name}, age = {age}");
+        }
+
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.SetName("Akshay");
+            p.SetAge(-1);
+            p.DisplayPersonDetails();
+        }
+    }
+    
+}
