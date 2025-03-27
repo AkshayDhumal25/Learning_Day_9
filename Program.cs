b6c3e0541921a0a09e13448bd66fd814af6c2b46
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-03-27 10:13:07 +0530

    Encapsulation with Properties

--- Program.cs
+++ Program.cs
@@ -1,54 +1,113 @@
 ﻿// Encapsulation in c#
+//using System;
+//namespace Encapsulation
+//{
+//    public class Person
+//    {
+//        private string name;
+//        private int age;
+
+
+//        public void SetName(string pName)
+//        {
+//            this.name = pName;
+//        }
+//        public string GetName()
+//        {
+//            return this.name;
+//        }
+
+//        public void SetAge(int pAge)
+//        {
+//            if(pAge > 0 && pAge < 150)
+//            {
+//                this.age = pAge;
+//            }
+//            else
+//            {
+//                throw new Exception("Invalid Age");
+//            }
+
+//        }
+//        public int GetAge()
+//        {
+//            return this.age;
+//        }
+//        public void DisplayPersonDetails()
+//        {
+//            Console.WriteLine($"name = {name}, age = {age}");
+//        }
+
+//    }
+
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Person p = new Person();
+//            p.SetName("Akshay");
+//            p.SetAge(-1);
+//            p.DisplayPersonDetails();
+//        }
+//    }
+
+//}
+
+
+// Encapsulation using Properties
 using System;
-namespace Encapsulation
+namespace EncapsulationWithProperties
 {
     public class Person
     {
         private string name;
         private int age;
+        private DateTime createdAt;
 
 
-        public void SetName(string pName)
+        public string Name
         {
-            this.name = pName;
-        }
-        public string GetName()
-        {
-            return this.name;
+            get { return name; }
+            set { name = value; }
         }
 
-        public void SetAge(int pAge)
+        public int Age
         {
-            if(pAge > 0 && pAge < 150)
+            get { return age; }
+            set 
             {
-                this.age = pAge;
+                if(value  > 0 && value <= 150)
+                {
+                    this.age = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid Age");
+                }
             }
-            else
-            {
-                throw new Exception("Invalid Age");
-            }
-            
         }
-        public int GetAge()
+        
+        public DateTime CreatedAt
         {
-            return this.age;
+            get { return createdAt; }
         }
-        public void DisplayPersonDetails()
+
+        
+        public Person()
         {
-            Console.WriteLine($"name = {name}, age = {age}");
+            this.createdAt = DateTime.Now;
         }
 
     }
 
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Person p = new Person();
-            p.SetName("Akshay");
-            p.SetAge(-1);
-            p.DisplayPersonDetails();
+            Person person = new Person();
+            person.Name = "Akshay";
+            person.Age = 21;
+            Console.WriteLine($"Name is {person.Name} and age is {person.Age}");
         }
     }
-    
 }
